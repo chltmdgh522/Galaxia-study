a1264f942e0be825f1cb69c43d9824b53d4f5d4c
chltm <chltmdgh517@naver.com>
2026-01-18 19:02:01 +0900

    초기 구조

---
+++ src/main/java/galaxia/GalaxiaApplication.java
@@ -0,0 +1,13 @@
+package galaxia.galaxia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/galaxia/GalaxiaApplicationTests.java
@@ -0,0 +1,13 @@
+package galaxia.galaxia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
